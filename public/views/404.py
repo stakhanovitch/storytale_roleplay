9adc8149fc64f493ac9cdaec932350131005530a
STKHVTCH <valentin.reverdy@gmail.com>
2018-09-05 17:08:38 +0200

    Update status to the proper HTTP error code

--- public/views/404.py
+++ public/views/404.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 
 def handler404(request):
-        data = {}
-        return render(request,'public/404.html', data)
+        return render(request,'public/404.html', status=404)
--- public/views/500.py
+++ public/views/500.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 
 def handler500(request):
-        data = {}
-        return render(request,'public/500.html', data)
+        return render(request,'public/500.html', status=404)
