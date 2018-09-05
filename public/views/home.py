f5766e35bbdbccef7ef3c92b15cbbfbc580a7fab
STKHVTCH <valentin.reverdy@gmail.com>
2018-09-05 12:40:31 +0200

    coquille

--- public/views/home.py
+++ public/views/home.py
@@ -17,6 +17,6 @@ class HomePageView(TemplateView):
         return context
     title = 'Storytale, Racontez une histoire qui vous ressemble'
     meta_title = title
-    meta_description = "Nous organisons des ateliers autour du jeu de rôle à destination des maitres du jeu pour les aider à s'améliore tout en se simplifiant la vie"
+    meta_description = "Nous organisons des ateliers autour du jeu de rôle à destination des maitres du jeu pour les aider à s'améliorer tout en leur simplifiant la vie"
     workshops = Workshop.objects.all()
     template_name = "public/home.html"
